bc7799a52464248e8bb15985d58099df035064ea
İlgün Fatma Oruç <ilgun.turkyilmaz@gmail.com>
2021-09-14 14:55:40 +0300

    MvcProjectCamp-14.09.2021

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAll()
+        {
+            //generic sınıfı içerisindeki tüm metotlar geliyor. repo. dediğimizde authentication işlemleri yapılabilir.
+            return repo.List();
+        }
+
+        public void CategoryAddBL(Category p)
+        {
+            if(p.CategoryName=="" || p.CategoryName.Length<=3 || p.CategoryDescription==""|| p.CategoryName.Length >= 51)
+            {
+                //hata mesajı
+                //hata mesajı için ayrı sınıf oluşturulmalı.
+            }
+            else
+            {
+                //Generic teki ekleme metodunu kullandık.
+                repo.Insert(p);
+            }
+        }
+    }
+}
---
+++ EntityLayer/Concrete/Heading.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Heading
+    {
+        [Key]
+        public int HeadingID { get; set; }
+
+        [StringLength(50)]
+        public string HeadingName { get; set; }
+
+        public DateTime HeadingDate { get; set; }
+
+        //The Relationship between Heading and Category 
+
+        public int CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+
+        //The Relationship between Heading and Content
+
+        public ICollection<Content> Contents { get; set; }
+
+
+        public int WriterID { get; set; }
+
+        public virtual Writer Writer { get; set; }
+
+    }
+}
